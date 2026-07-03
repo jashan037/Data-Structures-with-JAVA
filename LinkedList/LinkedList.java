21796ef4b57ef2e675470290e188cde0355015c1
Jashanpreet Singh <jashanpreetsingh3999@gmail.com>
2026-07-03 17:54:34 +0530

    Added insert in LinkedList

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -43,10 +43,29 @@ public class LinkedList<T> {
         }
     }
 
+    public void insert(T val, int ind) {
+        if (ind < 0 || ind > size)
+            throw new IllegalArgumentException("Illegal Index " + ind);
+        if (ind == 0) {
+            Node nd = new Node(val);
+            nd.next = Head.next;
+            Head = nd;
+        } else {
+            Node itr = Head;
+            while (ind != 1) {
+                itr = itr.next;
+                ind--;
+            }
+            Node sv = itr.next;
+            itr.next = new Node(val);
+            itr.next.next = sv;
+        }
+        size++;
+    }
+
     public T pop() {
         T ret = Tail.val;
-        Node itr = new Node();
-        itr = Head;
+        Node itr = Head;
         while (itr.next != Tail)
             itr = itr.next;
         itr.next = null;
@@ -56,8 +75,7 @@ public class LinkedList<T> {
 
     // Prints
     public void print() {
-        Node itr = new Node();
-        itr = Head;
+        Node itr = Head;
         while (itr != null) {
             System.out.print(itr.val);
             if (itr.next != null)
@@ -67,5 +85,4 @@ public class LinkedList<T> {
             itr = itr.next;
         }
     }
-
 }
